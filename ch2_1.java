e18a06547bbae1f7b07efb80a5c099ac303bc78f
victor20050102 <412631177@o365.tku.edu.tw>
2024-07-02 11:56:27 +0800

    我的第一支java

---
+++ ch2_1.java
@@ -0,0 +1,9 @@
+// ch2_1,簡單的java程式
+public class ch2_1 {      //定義public類別ch2_1
+    public static void main(String[] args) {    //main()函數函數,主要程式開始
+        int num1=2;       //宣告整數num
+        int num2=3;       //宣告整數num
+        System.out.println(num1+"cat are running");  //印出即便述內容
+        System.out.println(num2+"dogs are running"); //
+    }
+}
